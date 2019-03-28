19be543a4490308481300fc093b5d04ae9ae23f5
Rag <n.raghavendra2015@gmail.com>
2019-03-28 18:48:47 +0530

    New Example 13 and some changes to example 12

--- ex12.py
+++ ex12.py
@@ -3,3 +3,5 @@ height = input("How tall are you?")
 weight = input("How much do you weight?")
 
 print(f"So, you're {age} old, {height} tall, {weight} heavy")
+
+print("Hai?", input())
---
+++ ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+script, first, second, third = argv
+
+var1 = input("Just Type Something for Testing:")
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
+print("The variable inside program:", var1)
